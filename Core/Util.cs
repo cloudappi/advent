609cbdb5925546ff8c2bb299568267dc22e2f0d3
danijerez <danieljerezgarrido@gmail.com>
2021-11-23 10:44:24 +0100

    init

---
+++ Core/Util.cs
@@ -0,0 +1,12 @@
+﻿namespace advent.Core
+{
+    public class Util
+    {
+        public static int random(int total)
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, total);
+        }
+
+    }
+}
